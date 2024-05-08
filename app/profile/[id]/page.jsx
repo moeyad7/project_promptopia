11a14b9e4089ceba6b88924d4f4a06a82211ab91
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2024-05-08 14:41:43 +0300

    added suspense for searchparam

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -23,11 +23,13 @@ const UserProfile = ({ params }) => {
   }, [params.id]);
 
   return (
+    <Suspense>
     <Profile
       name={userName}
       desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
       data={userPosts}
     />
+    </Suspense>
   );
 };
 
